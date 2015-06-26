8709ffcccca6a6e9cfe08b54fbf8d2fcbcf50bc7
Durgesh Gupta <dgupta@steve.(none)>
2015-06-26 11:23:27 -0500

    :`

---
+++ DLWebServiceStructure/DLWebServiceStructure/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DLWebServiceStructure
+//
+//  Created by Durgesh Gupta on 6/26/15.
+//  Copyright © 2015 Durgesh Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
